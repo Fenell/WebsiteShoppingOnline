5149e7ddfe6af4160e7dc35e389c069a0ab9b967
DESKTOP-51CTS9F\LXMC <lxmchien@gmail.com>
2023-09-27 14:16:04 +0700

    AddApplicationServices

---
+++ ShoppingOnline.DAL/DataAccessServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingOnline.DAL.Database.AppDbContext;
+
+namespace ShoppingOnline.DAL;
+
+public static class DataAccessServiceRegistration
+{
+	public static IServiceCollection AddDataAccessLayerService(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDbContext<ApplicationDbContext>(otp =>
+			otp.UseSqlServer(configuration.GetConnectionString("ShoppingOnline")));
+
+		return services;
+	}
+}
