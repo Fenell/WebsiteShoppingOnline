02b45156fc11884526d2e18770483974ff0ac5e6
Tuấn Đạt <datmtph29475@fpt.edu.vn>
2023-09-29 01:50:46 +0700

    update solution

---
+++ ShoppingOnline.API/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingOnline.BLL.DataTransferObjects.CategoryDTO.Request;
+using ShoppingOnline.BLL.Features.CategoryFeature;
+
+namespace ShoppingOnline.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+	private readonly ICategoryService _categoryService;
+
+	public CategoriesController(ICategoryService categoryService)
+	{
+		_categoryService = categoryService;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Get()
+	{
+		var objColection = await _categoryService.GetAll();
+
+		return Ok(objColection);
+	}
+
+	[HttpGet]
+	[Route("{id}")]
+	public async Task<IActionResult> Get(Guid id)
+	{
+		var obj = await _categoryService.GetById(id);
+		return Ok(obj);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Create(CategoryCreateRequest request)
+	{
+		var result = await _categoryService.CreateCategory(request);
+
+		return Ok(result);
+	}
+
+	[HttpPut]
+	public async Task<IActionResult> Update(CategoryUpdateRequest request)
+	{
+		var result = await _categoryService.UpdateCategory(request);
+
+		return Ok(result);
+	}
+
+	[HttpDelete]
+	public async Task<IActionResult> Delete(Guid id)
+	{
+		var result = await _categoryService.DeleteCategory(id);
+
+		return Ok(result);
+	}
+
+}
