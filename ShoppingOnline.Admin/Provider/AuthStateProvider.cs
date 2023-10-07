f455945d50db64453997795d4bce5d5377ca0e3b
Mai Tuan Dat <datmtph29475@fpt.edu.vn>
2023-10-07 10:55:22 +0700

    login admin client done

---
+++ ShoppingOnline.Admin/Provider/AuthStateProvider.cs
@@ -0,0 +1,67 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ShoppingOnline.Admin.Provider;
+
+public class AuthStateProvider : AuthenticationStateProvider
+{
+	private readonly ILocalStorageService _localStorageService;
+	private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
+
+	public AuthStateProvider(ILocalStorageService localStorageService)
+	{
+		_localStorageService = localStorageService;
+		_jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+	}
+
+	public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+	{
+		var user = new ClaimsPrincipal(new ClaimsIdentity());
+		var exists = await _localStorageService.ContainKeyAsync("token");
+
+		if (!exists)
+			return new AuthenticationState(user);
+
+		var token = await _localStorageService.GetItemAsync<string>("token");
+		var tokenContent = _jwtSecurityTokenHandler.ReadJwtToken(token);
+		var userClaims = tokenContent.Claims.ToList();
+
+		userClaims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+
+		if (DateTime.UtcNow > tokenContent.ValidTo)
+			return new AuthenticationState(user);
+
+		user = new ClaimsPrincipal(new ClaimsIdentity(userClaims, "jwt"));
+
+		return new AuthenticationState(user);
+	}
+
+	public async Task LogedIn()
+	{
+		var listClaims = await GetClaimsAsync();
+		var user = new ClaimsIdentity(listClaims, "jwt");
+		var authState = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
+
+		NotifyAuthenticationStateChanged(authState);
+	}
+
+	public async Task LogedOut()
+	{
+		var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+		var authState = Task.FromResult(new AuthenticationState(anonymous));
+
+		NotifyAuthenticationStateChanged(authState);
+	}
+
+	private async Task<List<Claim>> GetClaimsAsync()
+	{
+		var token = await _localStorageService.GetItemAsync<string>("token");
+		var tokenContent = _jwtSecurityTokenHandler.ReadJwtToken(token);
+		var listClaims = tokenContent.Claims.ToList();
+		listClaims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+
+		return listClaims;
+	}
+}
--- ShoppingOnline.Admin/Services/Implement/AuthService.cs
+++ ShoppingOnline.Admin/Services/Implement/AuthService.cs
@@ -1,8 +1,46 @@
-﻿using ShoppingOnline.Admin.Services.Interface;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Newtonsoft.Json;
+using ShoppingOnline.Admin.Provider;
+using ShoppingOnline.Admin.Services.Interface;
+using ShoppingOnline.Admin.ViewModels.Auth;
+using System.Net.Http.Json;
 
 namespace ShoppingOnline.Admin.Services.Implement;
 
-public class AuthService:IAuthService
+public class AuthService : IAuthService
 {
-	
+	private readonly ILocalStorageService _localStorageService;
+	private readonly HttpClient _httpClient;
+	private readonly AuthenticationStateProvider _authState;
+
+	public AuthService(ILocalStorageService localStorageService, HttpClient httpClient, AuthenticationStateProvider authState)
+	{
+		_localStorageService = localStorageService;
+		_httpClient = httpClient;
+		_authState = authState;
+	}
+
+	public async Task<bool> Login(SignInVM vm)
+	{
+		var response = await _httpClient.PostAsJsonAsync("/api/Accounts/login", vm);
+		var responseContent = await response.Content.ReadAsStringAsync();
+
+		if (response.IsSuccessStatusCode)
+		{
+			var result = JsonConvert.DeserializeObject<SignInResponse>(responseContent);
+			await _localStorageService.SetItemAsync("token", result?.Token);
+			await ((AuthStateProvider)_authState).LogedIn();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public async Task Logout()
+	{
+		await _localStorageService.RemoveItemAsync("token");
+		await ((AuthStateProvider)_authState).LogedOut();
+	}
 }
